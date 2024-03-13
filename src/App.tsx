078cd918b2244a548fd5e24af938444ddd1400a9
Esther Lu <eslu@duosecurity.com>
2024-03-13 17:00:52 -0600

    add title (testing)

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App = (): React.ReactElement => {
     const [timerMinutes, setTimerMinutes] = React.useState(TIMER_DEFAULTS.work);
     return (
         <div className="App">
+            <h1>Clemmy Clock</h1>
             <button onClick={() => setTimerMinutes(TIMER_DEFAULTS.work)}>Work</button>
             <button onClick={() => setTimerMinutes(TIMER_DEFAULTS.shortBreak)}>Short Break</button>
             <button onClick={() => setTimerMinutes(TIMER_DEFAULTS.longBreak)}>Long Break</button>
