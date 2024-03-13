8bfc06269fe22f2a3d7cd737e3022d356899005d
Esther Lu <eslu@duosecurity.com>
2024-03-13 16:48:40 -0600

    demo buttons for different times

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyledCountdown } from './components/Countdown';
 
+const TIMER_DEFAULTS = {
+    work: 20,
+    shortBreak: 5,
+    longBreak: 15,
+}
+
 const App = (): React.ReactElement => {
+    const [timerMinutes, setTimerMinutes] = React.useState(TIMER_DEFAULTS.work);
     return (
         <div className="App">
-            <StyledCountdown timerMinutes={4} timerFor={'work'} />
-            <StyledCountdown timerMinutes={4} timerFor={'break'} />
+            <button onClick={() => setTimerMinutes(TIMER_DEFAULTS.work)}>Work</button>
+            <button onClick={() => setTimerMinutes(TIMER_DEFAULTS.shortBreak)}>Short Break</button>
+            <button onClick={() => setTimerMinutes(TIMER_DEFAULTS.longBreak)}>Long Break</button>
+            <StyledCountdown timerMinutes={timerMinutes} />
         </div>
   );
 }
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,41 +1,52 @@
 import React from 'react';
 import Countdown, { CountdownTimeDelta } from 'react-countdown';
 
-type TimerFor = 'work' | 'break';
-
-const TimerCompleted = ({ timerFor }: { timerFor: TimerFor }): React.ReactElement => {
-    return (
-        <>
-            {timerFor === 'work' && <p>Take a break!</p>}
-            {timerFor === 'break' && <p>Back to work!</p>}
-        </>
-    )
-}
-
 // renderer() must be a function, not a React component
 const renderer = ({ hours, minutes, seconds }: CountdownTimeDelta): React.ReactElement => {
+    const secondsString = seconds < 10 ? `0${seconds}` : seconds;
     return (
         <div>
-            <p>{hours} hours</p>
-            <p>{minutes} minutes</p>
-            <p>{seconds} seconds</p>
+            <p>{hours ? `${hours}:` : ''}{minutes}:{secondsString}</p>
         </div>
     );
 };
 
 type StyledCountdownProps = {
     timerMinutes: number;
-    timerFor: TimerFor;
 };
 
-export const StyledCountdown = ({ timerMinutes, timerFor }: StyledCountdownProps): React.ReactElement => {
+export const StyledCountdown = ({ timerMinutes }: StyledCountdownProps): React.ReactElement => {
     const [completed, setCompleted] = React.useState(false);
-    const countdownTimeMS = timerMinutes * 60 * 1000; // convert minutes to milliseconds
+    const [timerMilliseconds, setTimerMilliseconds] = React.useState(timerMinutes * 60 * 1000);
+    const countdownRef = React.useRef<Countdown>(null);
+
+    React.useEffect(() => {
+        // if timerMinutes changes, stop the clock and reset the countdown
+        if (countdownRef.current) {
+            countdownRef.current.stop();
+            setTimerMilliseconds(timerMinutes * 60 * 1000);
+        }
+    }, [timerMinutes])
+
+    const handleStart = (): void => {
+        if (countdownRef.current) {
+            countdownRef.current.start();
+        }
+    }
+
     return (
         <div>
-            <Countdown date={Date.now() + countdownTimeMS} renderer={renderer} onComplete={() => setCompleted(true)} />
+            <Countdown
+                ref={countdownRef}
+                date={Date.now() + timerMilliseconds}
+                autoStart={false}
+                renderer={renderer}
+                onComplete={() => setCompleted(true)}
+            />
 
-            {completed && <TimerCompleted timerFor={timerFor} />}
+            <button onClick={handleStart}>Start</button>
+
+            {completed && <p>Time's up!</p>}
         </div>
     );
 };
